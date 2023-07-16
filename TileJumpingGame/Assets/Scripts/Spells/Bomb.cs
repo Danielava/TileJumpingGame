7ce4a9a0d09602ab7a493a44bee390fb9ed9dcea
Daniel <dpar@kth.se>
2023-07-16 21:46:25 +0100

    Bomb projectile added (no explosion/damage graphics yet)

--- TileJumpingGame/Assets/Scripts/Spells/Bomb.cs
+++ TileJumpingGame/Assets/Scripts/Spells/Bomb.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class Bomb : Spell
 {
+    public BombObject m_BombObject;
+
     public override void InitSpell()
     {
         spellCastCost.Add(Element.Fire, 3);
@@ -16,6 +18,11 @@ public class Bomb : Spell
     {
         //TODO: Perform your spell here!
         GameObject.FindWithTag("Boss").GetComponent<Boss>().TakeDamage(20);
+
+        Vector3 pos = GameObject.FindWithTag("Player").transform.position;// + new Vector3(0, 0, -0.05f);
+        Tile playerTile = GameObject.FindWithTag("Player").GetComponent<Player>().CurrentTile;
+
+        Instantiate(m_BombObject, pos, Quaternion.identity).Init(playerTile);
         base.CastSpell();
     }
 }
